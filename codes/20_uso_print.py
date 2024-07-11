f6c8d9747b9e70068964ef59d7d0dd222c505b69
auphireo <auphireo@gmail.com>
2024-07-11 10:52:19 -0500

    se adjunta archivo '20_uso_print.py'

---
+++ codes/20_uso_print.py
@@ -0,0 +1,17 @@
+name = "Mario"
+edad = "25"
+
+print(name + ' : ' +edad)
+print(name, ':', edad)
+print(f'{name} : {edad}')
+print('%s : %s' % (name,edad))
+print('{} : {}'.format(name,edad))
+
+name1 = "Julio"
+edad1 = "35"
+
+print('La edad de ' + name1 + ' es ' +edad1 + 'años.')
+print('La edad de', name1, 'es', edad1, 'años')
+print(f'La edad de {name1} es {edad1} años')
+print('La edad de %s es %s años' % (name1,edad1))
+print('La edad de {} es {} años'.format(name1,edad1))
